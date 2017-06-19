73063907b82ffaee97c0b65ab7e7ad113bf88151
Javad Hosseini <hosseini.mohammadjavad@gmail.com>
2017-06-19 02:20:20 +0100

    more cleaning

---
+++ scripts/Double CRF_closed.sh
@@ -0,0 +1,26 @@
+set -eu
+type=3#Double CRF
+alpha=1.6
+alpha2=.12
+numIter=120
+itersMod=40#Saving outputs periodically after #itersMod iters
+cutoff=3
+
+outname=../dimsum-data-1.5/split/out$type-$alpha-$alpha2-$numIter-$cutoff.pred
+
+train=../dimsum-data-1.5/dimsum16.train
+test=../dimsum-data-1.5/dimsum16.test.gold
+
+#In case you're interested in the validation test in the paper:
+#train=../dimsum-data-1.5/split/dimsum16.train_allV
+#test=../dimsum-data-1.5/split/dimsum16.train_allT
+
+#Train and test for open condition:
+#python2.7 ../src/main.py --iters $numIter --itersMod 5 --cutoff $cutoff --YY ../tagsets/bio2gNV_dim --defaultY O --debug --train $train.tags --test-predict $test.tags --outFilePath $outname.tags --bio NO_SINGLETON_B --cluster-file ../mwelex/yelpac-c1000-m25.gz --clusters --lex ../mwelex/{semcor_mwes,wordnet_mwes,said,phrases_dot_net,wikimwe,enwikt}.json --ctype $type --alpha $alpha --alpha2 $alpha2
+
+#Train and test for closed condition:
+python2.7 ../src/main.py --iters $numIter --itersMod $itersMod --cutoff $cutoff --YY ../tagsets/bio2gNV_dim --defaultY O --debug --train $train.tags --test-predict $test.tags --outFilePath $outname.tags --bio NO_SINGLETON_B --cluster-file ../mwelex/yelpac-c1000-m25.gz --clusters --ctype $type --alpha $alpha --alpha2 $alpha2
+
+#The next two lines is for evaluation of the predicted tags
+python2.7 ../src/streusle2dimsum.py $outname.tags $outname
+python2.7 ../dimsum-data-1.5/scripts/dimsumeval.py $test $outname
